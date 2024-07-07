c7c8eae6a7088f67089fdc7a7b9571686a915b17
Himanshu Gupta <himanshugupta558@gmail.com>
2024-07-08 01:51:09 +0530

    added hollow rectangle pattern code

---
+++ Questions/1. Patterns/2.Hollow_Rectangle_Pattern.cpp
@@ -0,0 +1,38 @@
+/*
+    Hollow Rectangle Pattern:-
+
+    *****
+    *   *
+    *****
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rowCount, colCount;
+    cout << "Enter number of rows : ";
+    cin >> rowCount;
+    cout << "Enter number of cols : ";
+    cin >> colCount;
+
+
+    for(int row= 0; row < rowCount; row++)
+    {
+        if(row == 0 || row == rowCount - 1) {
+            for(int col = 0; col < colCount; col++) {
+                cout << "*";
+            }
+        }
+        else {
+            cout << "*";
+            for(int col = 0; col < colCount-2; col++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+
+    }
+    return 0;
+}

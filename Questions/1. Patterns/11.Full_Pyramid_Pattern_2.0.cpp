82d0b4d2f0154d47657b74e6dd5e7775f6a9c438
Himanshu Gupta <himanshugupta558@gmail.com>
2024-07-11 00:40:12 +0530

    added full pyramid pattern 2.0 code

---
+++ Questions/1. Patterns/11.Full_Pyramid_Pattern_2.0.cpp
@@ -0,0 +1,31 @@
+/*
+    Full Pyramid 2.0 :-
+
+      *  
+     * *
+    * * *
+   * * * *
+  * * * * *  
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter number of rows : ";
+    cin >> n;
+
+    for(int row = 0; row < n; row++) {
+        for(int col = 0; col < n - row - 1; col++) {
+            cout << " ";
+        }
+        for(int col = 0; col < row + 1; col++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}

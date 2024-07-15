f42a9e71416aff06f12b45395250cbea7d924448
Himanshu Gupta <himanshugupta558@gmail.com>
2024-07-15 19:55:47 +0530

    added solid half diamond pattern code

---
+++ Questions/1. Patterns/24.Solid_Half_Diamond_Pattern.cpp
@@ -0,0 +1,38 @@
+/*
+    Solid Half Diamond :-
+
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+    * * * *
+    * * *
+    * *
+    * 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of cols : ";
+    cin >> n;
+
+    for(int row = 0; row < 2*n - 1; row++) {
+
+        int condition = 0;
+        if(row < n) {
+            condition = row + 1;
+        }
+        else {
+            condition = n - (row % n) -1;
+        }
+        for(int col = 0; col < condition; col++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}

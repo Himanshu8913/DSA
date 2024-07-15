189582163aa2845d4f4afe988a300527a70400b3
Himanshu Gupta <himanshugupta558@gmail.com>
2024-07-15 19:58:25 +0530

    added code with ternary condition

--- Questions/1. Patterns/24.Solid_Half_Diamond_Pattern.cpp
+++ Questions/1. Patterns/24.Solid_Half_Diamond_Pattern.cpp
@@ -22,13 +22,15 @@ int main() {
 
     for(int row = 0; row < 2*n - 1; row++) {
 
-        int condition = 0;
-        if(row < n) {
-            condition = row + 1;
-        }
-        else {
-            condition = n - (row % n) -1;
-        }
+        // int condition = 0;
+        // if(row < n) {
+        //     condition = row + 1;
+        // }
+        // else {
+        //     condition = n - (row % n) -1;
+        // }
+
+        int condition = row < n ? row : n - (row % n) - 1;
         for(int col = 0; col < condition; col++) {
             cout << "*";
         }

8f629e2671baf053ba004ea5f3372aed2b345dc5
Himanshu Gupta <himanshugupta558@gmail.com>
2024-07-16 17:48:44 +0530

    added butterfly pattern code

---
+++ Questions/1. Patterns/30.Butterfly_Pattern.cpp
@@ -0,0 +1,54 @@
+/*
+    Butterfly Pattern
+
+    *                 *
+    * *             * *
+    * * *         * * *
+    * * * *     * * * *
+    * * * * * * * * * *
+    * * * * * * * * * *
+    * * * *     * * * *
+    * * *         * * *
+    * *             * *
+    *                 *
+    
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter value of n : ";
+    cin >> n;
+    int count = 1;
+
+    for(int row = 0; row < n; row++) {
+        for(int col = 0; col < row + 1; col++) {
+            cout << "*";
+        }
+        for(int col = 0; col < 2*n - 2*row - 2; col++) {
+            cout << " ";
+        }
+        for(int col = 0; col < row+1; col++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for(int row = 0; row < n; row++) { 
+        for(int col = 0; col < n-row; col++) {
+            cout << "*";
+        }
+        for(int col = 0; col < 2*row; col++) {
+            cout << " ";
+        }
+        for(int col = 0; col < n - row; col++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
